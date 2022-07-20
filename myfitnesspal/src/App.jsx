a80d5115251a814627970c1238e4ac1c04777a31
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-07-20 20:27:37 +0530

    day-2

--- myfitnesspal/src/App.jsx
+++ myfitnesspal/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import AllRoutes from './components/AllRoutes'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header/>
       <AllRoutes/>
     </div>
   )
